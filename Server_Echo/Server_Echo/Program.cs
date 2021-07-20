aa723b98267dd0e9c9df4ebd3512acb3d10b1c10
Xuhan <626150455@qq.com>
2021-07-20 10:08:48 +0800

    提交 Echo客户端和服务器 包含异步连接发送接收

---
+++ Server_Echo/Server_Echo/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Collections.Generic;
+namespace Server_Echo
+{
+    public class ClientState
+    {
+        public Socket Client;
+        public byte[] recvBuff = new byte[1024];
+    }
+
+    class Program
+    {
+        static Socket socket_Listen;
+
+        static Dictionary<Socket, ClientState> clientStateDict = new Dictionary<Socket, ClientState>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Server Echo");
+
+            socket_Listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress ipAdr = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEnd = new IPEndPoint(ipAdr, 8888);
+
+            socket_Listen.Bind(ipEnd);
+            socket_Listen.Listen(0);
+
+            socket_Listen.BeginAccept(AcceptCallback, socket_Listen);
+
+            // 这句必须有，否则命令行会直接关闭
+            Console.ReadLine();
+        }
+
+        static void AcceptCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket sk = ar.AsyncState as Socket;
+                Socket clientSocket = sk.EndAccept(ar);
+                ClientState clState = new ClientState();
+                clState.Client = clientSocket;
+                clientStateDict[clientSocket] = clState;
+                Console.WriteLine("Client 加入成功，端口号：" + (clientSocket.RemoteEndPoint as IPEndPoint).Port);
+                clState.Client.BeginReceive(clState.recvBuff, 0, 1024, 0, RecvCallback, clState);
+                sk.BeginAccept(AcceptCallback, sk);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static void RecvCallback(IAsyncResult ar)
+        {
+            try
+            {
+                ClientState clS = ar.AsyncState as ClientState;
+                Socket client = clS.Client;
+                int count = client.EndReceive(ar);
+
+                if (count == 0)
+                {
+                    client.Close();
+                    clientStateDict.Remove(client);
+                    Console.WriteLine($"客户端Socket关闭");
+                    return;
+                }
+
+                string recvStr = Encoding.Default.GetString(clS.recvBuff, 0, count);
+                Console.WriteLine($"接收Message：{recvStr}");
+
+                string sendStr = $"服务器收到Message{recvStr}";
+                byte[] sendBuff = Encoding.Default.GetBytes(sendStr);
+                client.Send(sendBuff);
+                Console.WriteLine($"服务器发送" + sendStr);
+
+                clS.Client.BeginReceive(clS.recvBuff, 0, 1024, 0, RecvCallback, clS);
+
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/Echo/EchoClient.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System;
+
+public class EchoClient : MonoBehaviour
+{
+    Socket socket;
+
+    public Button Btn_Connect;
+    public Button Btn_Send;
+    public InputField sendMessage;
+    public Text Txt_Recv;
+    //
+    private byte[] sendBuff = new byte[1024];
+    private byte[] recvBuff = new byte[1024];
+    private string sendStr;
+    private string recvStr;
+
+    private void Start()
+    {
+        Btn_Connect.onClick.AddListener(Connect);
+        Btn_Send.onClick.AddListener(Send);
+    }
+
+    public void Connect()
+    {
+        Debug.Log("主线程" + System.Threading.Thread.CurrentThread.ManagedThreadId);
+
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        // 同步
+        // socket.Connect("127.0.0.1", 8888);
+        // 异步
+        socket.BeginConnect("127.0.0.1", 8888, ConnectCallback, socket);
+    }
+
+    private void ConnectCallback(IAsyncResult ar)
+    {
+        try
+        {
+            Socket sk = (Socket)ar.AsyncState;
+            sk.EndConnect(ar);
+            Debug.Log("Socket Connect Succ");
+            sk.BeginReceive(recvBuff, 0, 1024, 0, RecvCallback, sk);
+
+        }
+        catch (SocketException se)
+        {
+            // 将会打印 由于目标计算机积极拒绝，无法连接。
+            Debug.Log(se);
+        }
+    }
+
+    private void RecvCallback(IAsyncResult ar)
+    {
+        try
+        {
+            Socket sk = (Socket)ar.AsyncState;
+            int count = sk.EndReceive(ar);
+            recvStr = Encoding.Default.GetString(recvBuff, 0, count);
+            Debug.Log("当前线程"+System.Threading.Thread.CurrentThread.ManagedThreadId);
+            //Txt_Recv.text = recvStr;
+            sk.BeginReceive(recvBuff, 0, 1024, 0, RecvCallback, sk);
+        }
+        catch(SocketException se)
+        {
+            Debug.Log(se);
+        }
+    }
+
+
+    public void Send()
+    {
+        sendStr = sendMessage.text;
+        sendBuff = Encoding.Default.GetBytes(sendStr);
+        socket.BeginSend(sendBuff, 0, sendBuff.Length, 0, SendCallback, socket);
+        Debug.Log("发送成功");
+    }
+
+    private void SendCallback(IAsyncResult ar)
+    {
+        try
+        {
+            Socket sk = ar.AsyncState as Socket;
+            int count = socket.EndSend(ar);
+            Debug.Log("成功发送" + count + "个字节"); 
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        socket?.Close();
+    }
+
+    private void Update()
+    {
+        Txt_Recv.text = recvStr;
+    }
+
+
+}
